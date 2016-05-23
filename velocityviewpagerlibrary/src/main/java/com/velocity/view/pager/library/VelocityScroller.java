a9f996cad0c3ce278f0a9068631177a183cfff7d
yangshuwei <yangshuwei@zenmen.com>
2016-05-23 15:09:21 +0800

    update for run success

--- velocityviewpagerlibrary/src/main/java/com/velocity/view/pager/library/VelocityScroller.java
+++ velocityviewpagerlibrary/src/main/java/com/velocity/view/pager/library/VelocityScroller.java
@@ -198,7 +198,7 @@ public class VelocityScroller {
     public float getCurrVelocity() {
         float squaredNorm = mScrollerX.mCurrVelocity * mScrollerX.mCurrVelocity;
         squaredNorm += mScrollerY.mCurrVelocity * mScrollerY.mCurrVelocity;
-        return FloatMath.sqrt(squaredNorm);
+        return (float) Math.sqrt(squaredNorm);
     }
 
     /**
